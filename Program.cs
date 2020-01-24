79c960a5d231b3922b5a136150d86cc87d6658f2
uday99 <udaypeddu9999@gmail.com>
2020-01-23 19:51:59 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
